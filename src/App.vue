85eb64407342f83af7429f44b16811fca886e491
Leslie Lau <1178273431@qq.com>
2025-06-05 10:08:53 +0800

    refactor ui with sidebar and shadcn-vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <RouterView />
+  <div class="layout">
+    <AppSidebar />
+    <main class="main">
+      <RouterView />
+    </main>
+  </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
+import AppSidebar from './components/AppSidebar.vue'
 </script>
 
 <style>
 body {
   margin: 0;
+}
+.layout {
+  display: flex;
+  min-height: 100vh;
+}
+.main {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
 }
 </style>
